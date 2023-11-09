4fcbcf51760ffd56385efa04f80a18808f303380
Sosus1009 <145317857+Sosus1009@users.noreply.github.com>
2023-11-09 14:07:27 +0900

    Title: 문자열 길이의 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231109/문자열 길이의 합/sum-length-of-string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    int n,i;
+    char str[100];
+    int len=0;
+    int cnt=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%s",str);
+        len+=strlen(str);
+        if('a'==str[0])
+        {
+            cnt++;
+        }
+    }
+    printf("%d %d",len,cnt);
+    return 0;
+}
